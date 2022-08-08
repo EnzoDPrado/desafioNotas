4048a9c751378d4e16d87814062abeb0920e1424
Enzo D. Prado <84857077+EnzoDPrado@users.noreply.github.com>
2022-08-08 11:14:29 -0300

    Add files via upload

--- exerciciosAula4/app.js
+++ exerciciosAula4/app.js
@@ -5,4 +5,48 @@
  *  Versão: 1.0
  *************************************************************************************/
 
+console.log('######### Sistemas para validar notas e cadastrar ########');
+
+//Importando biblioteca media que foi criada para functions
+const{ validarNotas }   = require('./modulos/media.js');
+const{ mediaCalculate } = require('./modulos/media.js');
+const{ mediaCheck }     = require('./modulos/media.js');
+const{ relatorioFinal } = require('./modulos/media.js');
+
+//Import da biblioteca da entrada de dados
+var readline = require('readline');
+
+//Instancia do objeto entradaDados
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+entradaDados.question('Insira o nome do aluno: ', function(nomeAluno){
+    entradaDados.question('Insira o sexo do aluno: ', function(alunoSexo){
+        alunoSexo.toLowerCase;
+            entradaDados.question('Insira o nome do professor: ', function(nomeProf){
+                entradaDados.question('Insira o sexo do professor: ', function(sexoProf){
+                    sexoProf.toLowerCase;
+                        entradaDados.question('Insira o nome do curso: ', function(nomeCurso){
+                            entradaDados.question('Insira as suas notas durante o curso, digite a 1: ', function(nota1){
+                                entradaDados.question('Insira a 2: ', function(nota2){
+                                    entradaDados.question('Insira a 3: ', function(nota3){
+                                        entradaDados.question('Insira a 4: ', function(nota4){
+                                            let check = validarNotas(nota1, nota2, nota3, nota4);
+                                            if(check == false){
+                                                console.log('Algum dos valores inseridos e invalido!')
+                                            }
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                })
+            })
+    })
+})
+
+
+
 
--- exerciciosAula4/modulos/media.js
+++ exerciciosAula4/modulos/media.js
@@ -1,25 +1,98 @@
-const ValidarNotas = function(nota1, nota2, nota3, nota4)
+const validarNotas = function(nota1, nota2, nota3, nota4)
 {
-    if((nota1 < 0 || nota1 > 100 || nota1 == null) || (nota2 < 0 || nota2 > 100) || (nota3 < 0 || nota3 > 100) || (nota4 < 0 || nota4 > 100)){
-        console.log('Alguma das notas nao foi inserida ou estao invalidas, inicie o programa novamente e tente de novo')
+    if((nota1 < 0 || nota1 > 100 || nota1 == null) || (nota2 < 0 || nota2 > 100 || nota2 == null) || (nota3 < 0 || nota3 > 100 || nota3 == null) || (nota4 < 0 || nota4 > 100 || nota4 == null)){
+        return true;
+    }
+
+    return false;
+
+}
+
+
+
+
+const mediaCalculate = function(val1, val2, val3, val4)
+{
+let media  = (val1 + val2 + val3 + val4) / 4;
+return media;
+}
+
+
+
+const mediaCheck = function(media)
+{
+    const { parse } = require('path');
+    const { exit } = require('process');
+    //Import da biblioteca da entrada de dados
+    var readline = require('readline');
+
+    //Instancia do objeto entradaDados
+    var entradaDados = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+
+    let situacao;
+
+    if(media > 70)
+    situacao = 'Aprovado!' 
+    else if(media >=50 || media <=69){
+    situacao = 'De exame'
+    console.log('faca o teste novamente para poder passar: ')
+    entradaDados.question('Digite sua nota no teste para passar', function(newVal){
+        parseFloat(newVal);
+        let newNota = (newVal + media) / 2;
+        if(newNota >= 60){
+        situacao = 'Aprovado!';
+        console.log(situacao);
+        }
+        else
+        situacao = 'Reprovado';
+    })
+
+    }
+    
+    else
+    situacao = 'Reprovado'
+
+
+    return situacao;    
+}
+
+
+const relatorioFinal = function(sexo, aluno, situacao, disciplina, curso, professor,sexoProfessor, nota1, nota2, nota3, nota4, notaDoExame, mediaFinal, mediaExame){
+    if(sexo == feminino){
+        console.log(`A aluna ${aluno} foi ${situacao} na disciplina ${disciplina} \n Curso: ${curso} \n `);
+        if(sexoProfessor == feminino)
+            console.log(`Professora: ${professor}`);
+        else
+            console.log(`Professor: ${professor}`);
+        if(notaDoExame == null || mediaExame == null)
+        console.log(`Notas da aluna: ${nota1} ${nota2} ${nota3} ${nota4} \n Media final: ${mediaFinal}`);
+        else
+        console.log(`Notas da aluna: ${nota1} ${nota2} ${nota3} ${nota4} ${notaDoExame} \n Media final: ${mediaFinal} \n Media final do Exame: ${mediaExame}`);
+    }
+    else{
+        console.log(`O aluno ${aluno} foi ${situacao} na disciplina ${disciplina} \n Curso: ${curso} \n `);
+        if(sexoProfessor == feminino)
+            console.log(`Professora: ${professor}`);
+        else
+            console.log(`Professor: ${professor}`);
+        if(notaDoExame == null || mediaExame == null)
+        console.log(`Notas do aluno: ${nota1} ${nota2} ${nota3} ${nota4} \n Media final: ${mediaFinal}`);
+        else
+        console.log(`Notas do aluno: ${nota1} ${nota2} ${nota3} ${nota4} ${notaDoExame} \n Media final: ${mediaFinal} \n Media final do Exame: ${mediaExame}`);
     }
 
 }
 
-const calcularMedia = function(nota1, nota2, nota3, nota4)
-{
-    let situacao;
-    let newExame;
-
-    let media = (nota1 + nota2 + nota3 + nota4) / 4;
-
-    if(media > 70)
-    situacao = 'Aprovado!' 
-    else if(media >=50 || media <=69)
-    situacao = 'De exame'
-    else
-    situacao = 'Reprovado' 
-    
+module.exports = {
+    validarNotas,
+    mediaCalculate,
+    mediaCheck,
+    relatorioFinal,
     
 }
+
 
